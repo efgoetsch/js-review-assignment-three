2044f28348e93927cad326cf8d7a1e8bf6ec6dcd
efgoetsch <egoetsch@mail.greenriver.edu>
2019-04-22 11:21:55 -0400

    debugging continues...

--- heeHawRevisited.js
+++ heeHawRevisited.js
@@ -8,12 +8,11 @@
 
 //prompts the user to pick a number (length for the number array)
 var size = prompt("Please enter a positive, whole number");
-                        //OR "Please set the size of the array, number list, etc..."
-                        //"you would like to create."
 
 //initializes an empty array
 var numbers = [];
 
+//creates an array using the 'size' specified by the user
 function makeArray()
 {
     //initializes a counter variable to keep track of how many times we have looped over the arra
@@ -21,27 +20,39 @@ function makeArray()
 
     for (i = 1; i <= size; i++) {
 
-        numbers.push(i);
+        numbers.push(i); //adds the elements to the array
     }
-    return numbers;
+    return numbers; //returns the array object for use later in the script.
 }
 
+//method which takes three properties on an array of numbers and uses them as parameters
+//from which a foreach loop can easily iterate over
 function heeHaw(currentValue, index, numbers) {
 
     numbers[index] = currentValue;
 
-    if (currentValue % 5 === 0 && currentValue % 3 === 0) {
-        console.log("Hee Haw!");
-    } else {
-        if (currentValue % 3 === 0) {
-            console.log("Hee! \n");
-        } else if (currentValue % 5 === 0) {
-            console.log("Haw! \n");
+        if (currentValue % 5 === 0 && currentValue % 3 === 0) {
+            console.log("Hee Haw!");
         } else {
-            console.log(currentValue + "\n");
+            if (currentValue % 3 === 0) {
+                console.log("Hee! \n");
+            } else if (currentValue % 5 === 0) {
+                console.log("Haw! \n");
+            } else {
+                console.log(currentValue + "\n");
+            }
         }
-    }
-
 }
-makeArray();
-numbers.forEach(heeHaw);
+
+
+
+if(!(Number.isNan(size)))
+{
+    makeArray();
+    numbers.forEach(heeHaw);
+}
+else
+{
+    console.log("Invalid entry: Please enter a positive, whole integer!");
+}
+
